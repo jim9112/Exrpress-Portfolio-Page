88327beefac5135aa4cc1efa83e70fb3570a1e63
jim9112 <jim9112@gmail.com>
2019-02-19 11:43:00 -0500

    Fourth Project added

--- routes/project.js
+++ routes/project.js
@@ -6,13 +6,15 @@ router.get('/:id', (req, res) => {
     const { id } = req.params;
     const projectName = projects[id].project_name;
     const projectDescription = projects[id].description;
+    const liveLink = projects[id].live_link;
+    const gitHubLink = projects[id].github_link;
     const projectTechnologies = projects[id].technologies;
     const projectImages = projects[id].image_urls;
     const imageGallery = [];
     for (i=1; i<projectImages.length; i++){
         imageGallery.push(projects[id].image_urls[i]);
     }
-    const templateData = { id, projectName, projectDescription, projectTechnologies, imageGallery};
+    const templateData = { id, projectName, projectDescription, projectTechnologies, imageGallery, gitHubLink, liveLink};
     res.render('project', templateData);
 });
 
