fe520fad7ed8522d23b5f967e28b95d16a40e0fa
jim9112 <jim9112@gmail.com>
2019-02-19 19:09:47 -0500

    Removed NPM addon that wasnt needed

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mainRoutes = require('./routes');
 const projectRoutes = require('./routes/project');
 const app = express();
 
-app.use(bodyParser.urlencoded( {extended: false}));
+// static server generation 
 app.use('/static', express.static('public'));
 
 app.set('view engine', 'pug');
 
+// routs for index and projects
 app.use(mainRoutes);
 app.use('/project', projectRoutes);
 
+
+// middleware for readable error
 app.use((req, res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
+// middleware to render error content
 app.use((err, req, res, next) => {
     res.locals.error = err;
     res.status(err.status);
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { projects } = require('../data.json');
 
+// Routing for the index or home screen
 router.get('/', (req, res) => {  
     const templateData = { projects };
     res.render('index', templateData);
 });
 
+// routing for the about page
 router.get('/about', (req, res) => {
     res.render('about');
 });
